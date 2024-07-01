7e8133c73883a2a00df89ff290a61d29040fec44
Michael Li <michaelli.fr@gmail.com>
2024-07-01 18:00:48 -0400

    fixing issues

--- script.js
+++ script.js
@@ -133,8 +133,6 @@ function active() {
   }
 }
 
-cntbtn.addEventListener("click", active);
-
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
   tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
